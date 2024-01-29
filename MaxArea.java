a72883b716b8dde117b0aa1c111e1316bf552c44
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-29 20:39:45 +0530

    Add files via upload

---
+++ MaxArea.java
@@ -0,0 +1,59 @@
+
+//Max Area in Histogram(bargraph) ?:- M,P,F
+import java.util.*;
+
+public class MaxArea {
+
+    public static void maxArea(int arr[]) {
+        int maxArea = 0;
+        int nsr[] = new int[arr.length];
+        int nsl[] = new int[arr.length];
+        // next smaller right = O(n)
+        Stack<Integer> s = new Stack<>();
+
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsr[i] = arr.length;
+            } else {
+                nsr[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // next smaller left
+        s = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            if (s.isEmpty()) {
+                nsl[i] = -1;
+            } else {
+                nsl[i] = s.peek();
+            }
+            s.push(i);
+        }
+
+        // current area:width = j-i-1 = nsr[i] - nsl[i]-1
+        for (int i = 0; i < arr.length; i++) {
+            int height = arr[i];
+            int width = nsr[i] - nsl[i] - 1;
+            int currArea = height * width;
+            maxArea = Math.max(maxArea, currArea);
+        }
+        System.out.println("max area in histogram = " + maxArea);
+    }
+
+    public static void main(String[] args) {
+        int arr[] = { 2, 1, 5, 6, 2, 3 };
+        maxArea(arr);
+    }
+}
+
+/*
+ * Output:-
+ * max area in histogram = 10
+ */
