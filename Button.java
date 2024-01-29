e40327c0d795aeded2fc7d1fa95630f8a8d8e573
Prakash-102 <142473108+Prakash-102@users.noreply.github.com>
2024-01-29 18:48:54 +0530

    Add files via upload

---
+++ Button.java
@@ -0,0 +1,32 @@
+
+//push at the Button of the stack ?:-amozone
+import java.util.*;
+
+public class Button {
+
+    public static void pushAtBottom(Stack<Integer> s, int data) {
+        if (s.isEmpty()) {
+            s.push(data);
+            return;
+        }
+        int top = s.pop();
+        pushAtBottom(s, data);
+        s.push(top);
+    }
+
+    public static void main(String[] args) {
+        Stack<Integer> s = new Stack<>();
+        s.push(1);
+        s.push(2);
+        s.push(3);
+        pushAtBottom(s, 3);
+        while (!s.isEmpty()) {
+            System.out.print(s.pop() + " ");
+        }
+    }
+}
+
+/*
+ * Output:-
+ * 3 2 1 3
+ */
